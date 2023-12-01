633f39d259ba304478df9dff25c99c8c464ba2d1
nashaat <ahmed195148@feng.bu.edu.eg>
2023-12-01 14:15:06 +0200

    Make STD_TYPES file

---
+++ COM module/Std_Types.h
@@ -0,0 +1,25 @@
+#ifndef STD_TYPES_H_
+#define STD_TYPES_H_
+
+
+
+typedef unsigned char			boolean;
+typedef unsigned char         	uint8;         
+typedef unsigned short        	uint16;        
+typedef unsigned long         	uint32;        
+typedef signed char           	sint8;       
+typedef signed short          	sint16;      
+typedef signed long           	sint32;         
+typedef signed long long      	sint64;
+typedef unsigned long long    	uint64;         
+typedef float                 	float32;
+typedef double                	float64;
+
+
+typedef enum
+{
+    E_OK,
+    E_NOT_OK,
+}Std_ReturnType;
+
+#endif 
