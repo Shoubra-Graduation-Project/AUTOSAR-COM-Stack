66075af64e8b4f8db88d4ea89e90241a559b4d6e
nashaat <ahmed195148@feng.bu.edu.eg>
2023-12-03 23:24:41 +0200

    Fix com.h file

--- COM module/Com.h
+++ COM module/Com.h
@@ -1,6 +1,23 @@
 #ifndef _COM_H_
-#define _COM_H_#define  COM_SERVICE_NOT_AVAILABLE 0x80#define COM_BUSY 0x81
-typedef unsigned short Com_SignalIdTypetypedef unsigned short Com_SignalGroupIdTypetypedef unsigned short Com_IpduGroupIdType
+#define _COM_H_
+
+#include "Std_Types.h"
+
+/* The service is currently not available */
+#define  COM_SERVICE_NOT_AVAILABLE 0x80
+
+/* Successful execution of this function is currently not possible */
+#define COM_BUSY 0x81
+
+/* The COM module's signal object identifier. */
+typedef uint16 Com_SignalIdType;
+
+/* The COM module's signal group object identifier */
+typedef uint16 Com_SignalGroupIdType;
+
+/* The COM I-PDU module's group object identifier. */
+typedef uint16 Com_IpduGroupIdType;
+
 /*
  This is a status value returned by the API service Com_GetStatus().
 */
@@ -9,3 +26,5 @@ typedef enum
  COM_UNINIT = 0x00,
  COM_INIT	
 }Com_StatusType;
+
+#endif
