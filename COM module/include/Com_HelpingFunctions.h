c9537c8c135cd79cfff249780c7d47225603bb8d
reem-mahmoud16 <66190640+reem-mahmoud16@users.noreply.github.com>
2024-01-25 16:36:43 +0200

    Update Com_HelpingFunctions.h

--- COM module/include/Com_HelpingFunctions.h
+++ COM module/include/Com_HelpingFunctions.h
@@ -4,4 +4,6 @@
 
 uint8 Com_ProcessTxSignalFilter(ComSignal_type* signalStruct, uint64 oldData, uint64 newData);
 
+uint8 Com_ProcessTxSignalFilter_float(ComSignal_type* signalStruct, float64 oldData, float64 newData);
+
 #endif
