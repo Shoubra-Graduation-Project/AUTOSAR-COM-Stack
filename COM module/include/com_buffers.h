4856e21c12308bcd292be897abefc21a5fe611bf
Aya-abdo27 <66348016+Aya-abdo@users.noreply.github.com>
2024-02-03 15:52:43 +0200

    Update com_buffers.h

--- COM module/include/com_buffers.h
+++ COM module/include/com_buffers.h
@@ -5,6 +5,7 @@
 void Com_CopyShadowBufferToIPDU (const Com_SignalGroupIdType signalGroupId);
 void Com_CopyPduToShadowBuffer(const Com_SignalGroupIdType signalGroupId) ;
 void CopyGroupSignalFromSBtoAddress(const ComSignalGroup_type SignalGroup_id,void *dataAddress);
-void CopySignalFromFGtoAddress(const ComSignalGroup_type Signal_id,void *dataAddress);
+void CopySignalFromFGtoAddress(const Com_SignalIdType Signal_id,void *dataAddress);
 void CopySignalGroupfromBGtoSB(Com_SignalGroupIdType SignalGroupId);
+void CopySignalfromBGtoFG(Com_SignalIdType Signal_id);
 #endif
