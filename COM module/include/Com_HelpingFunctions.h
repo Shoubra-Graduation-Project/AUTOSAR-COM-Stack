d7a111737b312ffff145a1028cbbd20baa91a3c4
reem-mahmoud16 <66190640+reem-mahmoud16@users.noreply.github.com>
2024-03-24 16:50:26 +0200

    Update Com_HelpingFunctions.h

--- COM module/include/Com_HelpingFunctions.h
+++ COM module/include/Com_HelpingFunctions.h
@@ -1,17 +1,17 @@
-
 #ifndef _COM_HELPINGFUNCTIONS_H_
 #define _COM_HELPINGFUNCTIONS_H_
 
 
-#include "Std_Types.h"
-#include "Com_types.h"
+#include "libraries/Std_Types.h"
+#include "include/com_types.h"
 
-
- 
-
-
-uint8 Com_ProcessTxSignalFilter(ComSignal_type* signalStruct, uint64 oldData, uint64 newData);
 uint8 power(unsigned n);
-uint8 Com_ProcessTxSignalFilter_float(ComSignal_type* signalStruct, float64 oldData, float64 newData);
-boolean check_Data_Sequence(ComIPdu_type *Ipdu,ComIPdu_type *Ipdu_Rx);
+boolean Com_ProcessTxSignalFilter(ComSignal_type* signalStruct, uint64 oldData, uint64 newData);
+boolean Com_ProcessTxSignalFilter_float(ComSignal_type* signalStruct, float64 oldData, float64 newData);
+uint8 com_pdu_transmissions_handle_signal(ComIPdu_type* IPdu, ComSignal_type* signal);
+uint8 com_pdu_transmissions_handle_signalGroup(ComIPdu_type* IPdu, ComSignalGroup_type* signalGroup);
+boolean com_pdu_transmissionsModeSelection(ComIPdu_type* IPdu);
+void com_packSignalsToPdu(ComIPdu_type* IPdu);
+Std_ReturnType Com_writeCounterValueToPduBuffer(ComIPdu_type *IPdu, uint8 counterdata);
+uint8 check_Data_Sequence(ComIPdu_type *Ipdu);
 #endif
