b3b85c73c6612ea106700db78fc2395217948532
reem-mahmoud16 <reem195398@feng.bu.edu.eg>
2024-05-29 18:34:16 +0300

    test periodic

---
+++ Timer Driver/include/ComStack_Types.h
@@ -0,0 +1,34 @@
+#ifndef _COM_STACK_TYPES_H
+#define _COM_STACK_TYPES_H
+
+/**********************************************************
+ * 
+ *                     Includes
+ * 
+ **********************************************************/
+#include "Std_Types.h"
+
+typedef uint16 PduIdType;
+typedef uint32 PduLengthType;
+
+typedef struct 
+{
+uint32 SduLength;
+void *SduDataPtr;
+  
+}PduInfoType;
+
+
+
+
+typedef enum
+{
+    BUFREQ_OK,                           //: success
+    BUFREQ_E_NOT_OK,                    //: development error detected
+    BUFREQ_E_OVFL                      // : requested buffer too large
+    
+}BufReq_ReturnType ;
+
+
+
+#endif
